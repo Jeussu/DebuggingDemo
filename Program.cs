09b64538db447015c452f0162fc9f2bb3c714bc9
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-10-11 09:46:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+
+namespace Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var friends = new List<string> { "Frank", "Joe", "Michelle", "Andy", "Maria", "Carlos", "Angelina" };
+            //var friends = new List<string>();
+            var partyFriends = GetPartyFriends(friends, 10);
+
+            foreach (var name in partyFriends)
+                Console.WriteLine(name);
+        }
+
+        public static List<string> GetPartyFriends(List<string> list, int count)
+        {
+            if (list == null)
+                throw new ArgumentNullException("List", "The list is empty");
+
+
+            if (count > list.Count || count <= 0)
+                throw new ArgumentOutOfRangeException("count", "count cannot be greater then elements in the list or lower 0");
+
+
+
+            var buffer = new List<string>(list);
+            var partyFriends = new List<string>();
+
+            while (partyFriends.Count < count)
+            {
+                var currentFriend = GetPartyFriend(buffer);
+                partyFriends.Add(currentFriend);
+                buffer.Remove(currentFriend);
+            }
+            return partyFriends;
+        }
+        /// <summary>
+        /// This is the logic to figure out who is a party 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static string GetPartyFriend(List<string> list)
+        {
+            string shortestName = list[0];
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length < shortestName.Length)
+                {
+                    shortestName = list[i];
+                }
+            }
+            return shortestName;
+        }
+    }
+}
+
+
+
+
+
+
+
